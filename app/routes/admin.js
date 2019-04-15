43be722da52cde4dcc0a5cd9561f645e877f7167
Lucas.Fernandes <lucas.fernandes001p@gmail.com>
2019-04-14 23:28:14 -0300

    Utilizando Express Validator para Validação de Formulario

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,21 @@ module.exports = function(app){
 
     app.post('/noticias/salvar', function(req, res){
         var noticia = req.body;
+
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100); //só pode ter entre 10 e 100 caracteres
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        req.assert('noticia', 'Noticia é obrigatória').notEmpty();
+
+        var erros = req.validationErrors();
+
+        if(erros){
+            res.render("admin/form_add_noticia");
+            return;
+        }
+
         var connection = app.config.dbConnection();
         var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
